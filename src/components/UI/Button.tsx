1ab679ececffa687d6a23d70ce4eeee8c4c4b335
Violeta Taseva <87774260+tasevavi@users.noreply.github.com>
2023-09-14 16:29:25 +0300

    Fix btn styling

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -4,12 +4,17 @@ import './Button.css'
 // TODO: replace any with smth else
 
 function Button(props: any): JSX.Element {
-    const label = props.btnProps.text
+    const label = props.props.text
+    const style = props.props.class
     console.log(props)
     const clickHandler = () => {
         console.log('btn clicked')
     }
-    return <button onClick={clickHandler}>{label}</button>
+    return (
+        <button className={style} onClick={clickHandler}>
+            {label}
+        </button>
+    )
 }
 
 export default Button
